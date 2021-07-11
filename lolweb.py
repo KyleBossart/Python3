8818fa3cd60894c3ab16d9d6b50ed2a1f189f711
kyle <kbossart95@gmail.com>
2021-07-10 19:21:55 -0700

    Commit

--- lolweb.py
+++ lolweb.py
@@ -1,9 +1,23 @@
 from tkinter import *
-
 root = Tk()
+#
 
-myLabel = Label(root, text="Hello World!")
+e = Entry(root, bg="lightblue", width=200, borderwidth=5)
+e.pack()
+e.get()
+e.insert(0, "Enter your name.")
 
-myLabel.pack()
 
+def myClick():
+    name = "Hello " + e.get() + "."
+    myLabel = Label(root, text=name)
+    myLabel.pack()
+
+myButton = Button(root, text="Enter your name.", padx=50, pady=20, command=myClick, bg="#531235")
+myButton.pack()
+
+
+
+
+#MAIN RUNNING LOOP
 root.mainloop()
