2309204d1ee37286d1f46f45a143d64a440231d3
kyle <kbossart95@gmail.com>
2021-07-11 20:38:48 -0700

    commit

--- adventuregame.py
+++ adventuregame.py
@@ -5,25 +5,21 @@ import time
 hp = 10
 dmg = 1
 potion = 5
+inventory_potion = 0
 
 # Functions
 system("clear")
 
 def wait():
-    time.sleep(1)
+    time.sleep(1.5)
 
 def game_over():
     print("GAME OVER")
     quit()
 
-def take_potion():
-    hp += 5
-
-def take_damage():
-    hp -= 1
-
 def total_health():
-    print("Current Health: " + str(hp))
+    print("Current Health: " + str(hp) + "/10.")
+
     
 
 # Want to play?
@@ -34,42 +30,65 @@ if start.lower() != "yes":
 else:
     print("Okay, let's get started!")
     wait()
+    print("")
+    print("")
 
 # Part 1
 print("You wake up in a dark room with a single door.")
 wait()
-part1 = input("What would you like to do? ")
+part1 = input("What would you like to do? > ")
 if part1.lower() != "open the door":
+    wait()
     print("When you attempt to " + str(part1.lower()) + " a rat kills you.")
+    wait()
     game_over()
 else:
-    print("You open the door and feel a cold breeze.")
+    print("")
+    print("You open the door and feel a cold breeze... \n")
+    wait()
     wait()
 
 # Part 2
-print("You take a step outside and find yourself in a dark forest.")
+print("You take a step outside and find yourself in a dark forest. \n")
 wait()
-print("You see a rusty sword sitting on the ground and you pick it up.")
+print("You see a rusty sword sitting on the ground and you pick it up. \n")
 wait()
-print("As you pick up the sword you notice a small creature running at you.")
+print("As you pick up the sword you notice a small creature running at you. \n")
 wait()
 
 # Part 2 - Attack or Run - Creature and Level Up
-atk_or_run = input("Do you attack the creature with the sword or run? ")
+atk_or_run = input("Do you attack the creature with the sword or run? > ")
 if atk_or_run.lower() == "attack":
-    print("You swing the sword at the creature and kill it, but take some damage in the process.")
+    print("")
+    print("You swing the sword at the creature... \n")
     wait()
-    take_damage()
-    print("-5 Health. Total Health: " + total_health())
     wait()
-    print("After killing the creature you feel stronger.")
+    print("and kill it! But you take some damage in the process. \n")
     wait()
-    print("Congratulations! You are level 2! Damage increased to 2.")
+    hp -= 5
+    print(">> -5 Health.")
+    total_health()
+    print("")
+    wait()
+    print("After killing the creature you feel stronger... \n")
+    wait()
+    wait()
+    print("Congratulations! You are level 2! Damage increased to 2. \n")
     dmg += 1
+    wait()
 
 else:
     print("You run from the creature, but it catches up and kills you.")
     game_over()
 
 # Part 3
-print("After killing the creature, you search it and find a small health potion")
+print("You search the creature's corpse and find a small health potion \n")
+inventory_potion += 1
+drink = input("Do you want to drink the potion and restore some health? >  ")
+if drink.lower() == "yes":
+    wait()
+    print(" \nYou drink the potion and feel rejuvenated.")
+    inventory_potion -= 1
+    hp += 5
+    wait()
+    total_health()
